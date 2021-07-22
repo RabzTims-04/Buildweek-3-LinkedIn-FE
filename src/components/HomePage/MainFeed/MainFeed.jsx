8bac3981e3aacbc7e1f050a049bea94f821a9991
Rabz92 <rabbiailyas92@gmail.com>
2021-07-22 16:40:24 +0200

    likes implemented

--- src/components/HomePage/MainFeed/MainFeed.jsx
+++ src/components/HomePage/MainFeed/MainFeed.jsx
@@ -35,6 +35,8 @@ const MainFeed = ({post, user, editPost, editPostImg, postDelete}) => {
   const [editComments, setEditComments] = useState();
   const objComm = {text: editComments}
   const [commentId, setCommentId] = useState();
+  const [likes, setLikes] = useState(false);
+  const [likeData, setLikeData] = useState();
   // function to edit post
 
   function EditPost() {
@@ -223,6 +225,25 @@ const MainFeed = ({post, user, editPost, editPostImg, postDelete}) => {
     }
   }
 
+  async function postLikes() {
+    try {
+      const response = await fetch(`${REACT_APP_BACKEND_URL}/posts/${user._id}/likes/${post._id}`, {
+        method:"POST",
+        headers:{
+          "content-type":"application/json"
+        }
+      })
+      const data = await response.json();
+      console.log(data);
+      if(response.ok){
+        setLikes(!likes)
+        setLikeData(data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const dateFunction = (dateVal) => {
     const today = new Date()
     if(dateVal.getDate() === today.getDate()){
@@ -265,7 +286,7 @@ const MainFeed = ({post, user, editPost, editPostImg, postDelete}) => {
                         <img src="https://static-exp1.licdn.com/sc/h/54ivsuv8nxk12frsw45evxn3r" alt="APPRECIATION"/> 
 
                        
-                        <span style={{fontSize:'0.7em'}} className="text-muted ml-2 number-span"> <Link style={{fontSize:'1em', fontWeight:'450'}} className="text-muted" to="">321</Link> </span>
+                        <span style={{fontSize:'0.7em'}} className="text-muted ml-2 number-span"> <Link style={{fontSize:'1em', fontWeight:'450'}} className="text-muted" to="">{likeData && likeData.likes.length}</Link> </span>
                         
 
                         <span style={{fontSize:'0.7em'}} className="text-muted ml-2"> <Link style={{fontSize:'1em', fontWeight:'450'}} className="text-muted" to="">{comments && comments.length} {comments && comments.length === 1? "comment":"comments"}</Link> </span>                    
@@ -273,9 +294,11 @@ const MainFeed = ({post, user, editPost, editPostImg, postDelete}) => {
         {post.user._id === "60f67bd86bce175ba8dec1d7" ? (
          
             <div className="MainFeedBodyButtons text-muted ">
+              <div id= {likes ? "likeone" : ""}  onClick={postLikes}>
               <InputOptions SVGs={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match m-0" width="24" height="24" focusable="false">
                                 <path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
-                                </svg>} title="Like" />
+                                </svg>} title={likes ? "Liked" : "Like"} />
+              </div>
           <Accordion.Toggle
             id="accordion_toggle_button"
             as={Button}
@@ -303,9 +326,11 @@ const MainFeed = ({post, user, editPost, editPostImg, postDelete}) => {
          
         ) : (
           <div className="MainFeedBodyButtons text-muted ">
+            <div id= {likes ? "like" : ""} onClick={postLikes}>
               <InputOptions SVGs={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match m-0" width="24" height="24" focusable="false">
                                 <path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
-                                </svg>} title="Like" />
+                                </svg>} title={likes ? "Liked" : "Like"} />
+                </div>
           <Accordion.Toggle
             id="accordion_toggle_button"
             as={Button}
